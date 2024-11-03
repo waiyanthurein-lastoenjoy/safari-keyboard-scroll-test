1b6568d09485a0115c96bbfd45d01ad07542809a
WaiYanThuRein <waiyanthurein.lastoenjoy@gmail.com>
2024-11-03 09:33:48 +0700

    version 3

--- components/Home.js
+++ components/Home.js
@@ -3,8 +3,8 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className='bg-gray-400 max-w-[480px] w-full max-h-[100dvh] h-[100dvh] mx-auto  relative overflow-hidden'>
-      <p className='mt-1 text-white absolute top-1 left-0 max-w-[480px] w-full text-[20px] font-bold text-center'> Version 2  </p>
+    <div className='bg-gray-400 max-w-[480px] w-full  h-[100%] mx-auto  relative overflow-hidden'>
+      <p className='mt-1 text-white absolute top-1 left-0 max-w-[480px] w-full text-[20px] font-bold text-center'> Version 3  </p>
 
       <div className='flex flex-col gap-y-1 w-full px-1 mt-12 overflow-scroll h-[calc(100%-40px)]'>
       {Array.from({ length: 50 }, (_, index) => (
@@ -24,3 +24,6 @@ const Home = () => {
 }
 
 export default Home
+
+//version 2 result
+// scroll still open, but 
